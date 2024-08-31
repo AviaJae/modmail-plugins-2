f0f2481c16dbae5f8129e32d6b9d1b33202f2ca1
AviaJae <81942396+AviaJae@users.noreply.github.com>
2024-08-31 16:59:01 +0800

    Update config.py

--- openai/config.py
+++ openai/config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config:
-    OPENAI_API_KEY = os.getenv('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')
+    OPENAI_API_KEY = os.getenv('')
